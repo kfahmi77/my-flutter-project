2e5e6ee602edd1db23ea5ca86b624f3e0bd11f9d
khoirulfahmi44 <khoirulfahmi44@gmail.com>
2024-07-29 10:18:07 +0000

    create default branch main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-import 'leaderboard_view.dart';
-import 'play_menu_view.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
-    runApp(const MyApp());
-  });
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,87 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Tetris',
+      title: 'Flutter Main',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const MainMenuPage(),
-        '/game': (context) => const PlayMenuView(),
-        '/leaderboard': (context) => const LeaderboardView(),
-      },
+      home: const MainView(),
     );
   }
 }
 
-class MainMenuPage extends StatelessWidget {
-  const MainMenuPage({super.key});
+class MainView extends StatelessWidget {
+  const MainView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF2C3E50), Color(0xFF34495E)],
-          ),
-        ),
-        child: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'TETRIS',
-                  style: GoogleFonts.pressStart2p(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                 Text(
-                  'Build ❤️ Flutter ',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.pressStart2p(
-                    fontSize: 18,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 60),
-                _buildMenuButton(
-                    "Play Game", () => Navigator.pushNamed(context, '/game')),
-                const SizedBox(height: 20),
-                _buildMenuButton(
-                    "Leaderboard", () => Navigator.pushNamed(context, '/leaderboard')),
-              ],
-            ),
-          ),
-        ),
+      appBar: AppBar(
+        title: const Text('Main View'),
       ),
-    );
-  }
-
-  Widget _buildMenuButton(String text, VoidCallback onPressed) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFF3498DB),
-        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-      ),
-      child: Text(
-        text,
-        style: GoogleFonts.roboto(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
+      body: const Center(
+        child: Text('This is the main view.'),
       ),
     );
   }
